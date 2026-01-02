75a46f3686d3656d831356610e6cf47a8bda04ac
abhishek123-cmyk <134828971+abhishek123-cmyk@users.noreply.github.com>
2026-01-02 15:40:18 +0530

    Use latest Ubuntu AMI for EC2 instance
    Updated EC2 instance to use the latest Ubuntu AMI.

--- ec2-creation-in-stockholm-region/main.tf
+++ ec2-creation-in-stockholm-region/main.tf
@@ -2,12 +2,16 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "github_ec2" {
-ami = "ami-0a4408457f9a03be3"
-
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "EC2-from-GitHub-Actions"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Ubuntu official owner
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+}
+
+resource "aws_instance" "github_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 }
